ed2eb5955bafa052d2f3f68361296d5e4e038d8e
k1complete <oda@maqcsa.net>
2013-02-11 09:45:02 +0900

    for iex 0.8.0

--- lib/app.ex
+++ lib/app.ex
@@ -1,9 +1,13 @@
 defmodule App do
   use Application.Behaviour
 
+  def start do
+    Application.Behaviour.start(:app)
+  end
   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
   # for more information on OTP Applications
-  def start(_type, _args) do
-    App.Supervisor.start_link nil
+  def start(_type, args) do
+    # args is arg parameter in .app file
+    App.Supervisor.start_link(args)
   end
 end
